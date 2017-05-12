1b91fab12afece22d7721969ab972acc1acc1b44
juggernautjosh5 <joshuasfuller@yahoo.com>
2017-05-12 10:43:45 -0700

    Joshua Fuller 5/12 update
    Added the table and some values

--- myContactApp/app/src/main/java/com/example/macbookstudiopro/mycontactapp/DataBaseHelper.java
+++ myContactApp/app/src/main/java/com/example/macbookstudiopro/mycontactapp/DataBaseHelper.java
@@ -3,6 +3,7 @@ package com.example.macbookstudiopro.mycontactapp;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.ActionBar;
 
 /**
  * Created by MacbookStudioPro on 5/11/17.
@@ -15,17 +16,19 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     public static final String COL_1 = "ID";
     public static final String COL_2 = "Name";
 
-     public DataBaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
-        super(context, DATABASE_NAME, null, version);
+    public DataBaseHelper(Context context){
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID_INTEGER_PRIMARY_KEY_AUTOINCREMENT, NAME TEXT)");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 }
