607616fed4cb3f321b913b8c85d3d32a6a404bba
juggernautjosh5 <joshuasfuller@yahoo.com>
2017-05-15 11:07:36 -0700

    Joshua Fuller 5/15
    Updates

--- myContactApp/app/src/main/java/com/example/macbookstudiopro/mycontactapp/DataBaseHelper.java
+++ myContactApp/app/src/main/java/com/example/macbookstudiopro/mycontactapp/DataBaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.macbookstudiopro.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -15,6 +16,9 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "Name";
+    public static final String COL_3 = "Age";
+    public static final String COL_4 = "Gender";
+
 
     public DataBaseHelper(Context context){
         super(context, DATABASE_NAME, null, 1);
@@ -22,7 +26,7 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID_INTEGER_PRIMARY_KEY_AUTOINCREMENT, NAME TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
 
     }
 
@@ -30,5 +34,18 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+
+    public boolean insertData(String name, String age, String gender){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, age );
+        contentValues.put(COL_4, gender );
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) return false;
+        else return true;
     }
 }
--- myContactApp/app/src/main/java/com/example/macbookstudiopro/mycontactapp/MainActivity.java
+++ myContactApp/app/src/main/java/com/example/macbookstudiopro/mycontactapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.macbookstudiopro.mycontactapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DataBaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editGender;
     Button btnAddData;
 
 
@@ -18,5 +24,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DataBaseHelper(this);
+
+        //Add the layout bars
+        editName = (EditText) findViewById(R.id.editText_Name);
+        editAge = (EditText) findViewById(R.id.editText_Age);
+        editGender = (EditText) findViewById(R.id.editText_Gender);
+    }
+
+    public void addData(View v){
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editAge.getText().toString(), editGender.getText().toString());
+
+        if(isInserted == true){
+            Log.d("MyContact", "Success inserting data");
+            //Insert Toast mesage here.......
+            Toast.makeText(v.getContext(), "You did it!", Toast.LENGTH_LONG).show();
+        } else{
+            Log.d("MyContact", "Failure inserting data");
+            //Insert Toast mesage here.......
+            Toast.makeText(v.getContext(), "You didn't do it!", Toast.LENGTH_LONG).show();
+        }
     }
 }
