c9d778f58fbe9cb94e31e548cc56fd22cf02b210
juggernautjosh5 <joshuasfuller@yahoo.com>
2017-05-17 10:02:58 -0700

    Update 5/17 Joshua Fuller

--- myContactApp/app/src/main/java/com/example/macbookstudiopro/mycontactapp/DataBaseHelper.java
+++ myContactApp/app/src/main/java/com/example/macbookstudiopro/mycontactapp/DataBaseHelper.java
@@ -2,6 +2,7 @@ package com.example.macbookstudiopro.mycontactapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.ActionBar;
@@ -47,5 +48,11 @@ public class DataBaseHelper extends SQLiteOpenHelper{
         long result = db.insert(TABLE_NAME, null, contentValues);
         if(result == -1) return false;
         else return true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
     }
 }
--- myContactApp/app/src/main/java/com/example/macbookstudiopro/mycontactapp/MainActivity.java
+++ myContactApp/app/src/main/java/com/example/macbookstudiopro/mycontactapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.macbookstudiopro.mycontactapp;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.database.Cursor;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.StringBuilderPrinter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editName;
     EditText editAge;
     EditText editGender;
-    Button btnAddData;
+    String[] fields;
 
 
     @Override
@@ -43,5 +47,37 @@ public class MainActivity extends AppCompatActivity {
             //Insert Toast mesage here.......
             Toast.makeText(v.getContext(), "You didn't do it!", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void viewData(){
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0){
+            //It's empty
+            showMessage("Error", "No Data Found in DataBase");
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        res.moveToFirst();
+        //SetUp Loop with Coursor using moveToNext
+        for(int i = 0; i<res.getCount(); i++){
+            for(int j = 1; j<=3; j++){
+                buffer.append(fields[j-1]);
+                buffer.append(res.getString(j));
+                buffer.append("\n");
+            }
+            res.moveToNext();
+        }
+
+
+        showMessage("Data Found", buffer.toString());
+        System.out.println(buffer);
+    }
+
+    public void showMessage(String error, String s){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setCancelable(true);
+        alertDialogBuilder.setTitle(error);
+        alertDialogBuilder.setMessage(s);
+        alertDialogBuilder.show();
     }
 }
